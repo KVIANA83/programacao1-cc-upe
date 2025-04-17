de9fcebe5d54d17fc6405043b62c75ae22b3a2c7
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2025-04-17 08:33:25 -0300

    Criar aula3/varstring.py
    adicionando o script

---
+++ aula3/varstring.py
@@ -0,0 +1,21 @@
+# Aula 3 - Tipos de Dados e Variáveis
+
+# Questão 1
+nome = "Karine"
+print(nome)
+
+# Questão 2
+frase = "O Senhor é o meu pastor e nada me faltará. Salmos 23"
+print(frase)
+
+# Questão 3
+palavra = "chocolate" 
+print(palavra[0])
+
+# Questão 4
+print(frase.upper())
+
+# Questão 5
+endereco = "Rua das Flores, 123"
+endereco = "Avenida Brasil, 456"
+print(endereco)
