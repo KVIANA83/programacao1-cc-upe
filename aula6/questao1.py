ae8c254e4371e9ec437432fdc37a709b2899c10d
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2025-05-24 14:45:22 -0300

    Criar aula6/questao1.py
    adicionando script

---
+++ aula6/questao1.py
@@ -0,0 +1,7 @@
+# Crie uma variável chamada "array" e atribua a ela um array vazio. 
+# Verifique o tipo de dado dessa variável usando "type"
+
+# 1. Criação da variável e verificação do tipo
+array = []
+print("Tipo da variável 'array':", type(array))
+
