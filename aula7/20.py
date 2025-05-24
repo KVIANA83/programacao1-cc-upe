f5e717db56087effdc5da6ba418f7fc2d97f1d41
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2025-05-24 15:34:10 -0300

    Atualizar o 20.py

--- aula7/20.py
+++ aula7/20.py
@@ -1,13 +1,13 @@
-# 20. Menor valor positivo e Ã­mpar
+# 20. Menor valor positivo e ímpar
 menor = None
 while True:
-    n = int(input("Digite um nÃºmero (0 para sair): "))
+    n = int(input("Digite um número (0 para sair): "))
     if n == 0:
         break
     if n > 0 and n % 2 == 1:
         if menor is None or n < menor:
             menor = n
 if menor is not None:
-    print("Menor valor positivo e Ã­mpar:", menor)
+    print("Menor valor positivo e ímpar:", menor)
 else:
-    print("Nenhum valor positivo e Ã­mpar digitado.")
+    print("Nenhum valor positivo e ímpar digitado.")
