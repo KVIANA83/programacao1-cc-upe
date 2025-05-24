fc529f9c2a0a5d9242fffe48f7035182d427b01a
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2025-05-24 15:57:29 -0300

    Criar 1e2.py

---
+++ aula7/1e2.py
@@ -0,0 +1,7 @@
+# 1. Diferença entre for e while:
+# 'for' é usado quando sabemos o número de iterações.
+# 'while' é usado quando não sabemos quantas vezes o loop será executado.
+
+# 2. Circunstâncias:
+# Use 'for' quando houver uma contagem conhecida.
+# Use 'while' quando a repetiçãoo depender de uma condição que será verificada durante a execução.
